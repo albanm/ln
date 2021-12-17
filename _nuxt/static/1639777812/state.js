1104f7efec9a9b8ec61015a1e523889589b74c2f
albanm <albanm@users.noreply.github.com>
2021-12-17 21:50:13 +0000

    deploy: 9603ea0a65ddcda733d933efb619d607e588682c

---
+++ _nuxt/static/1639777812/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fln\u002F_nuxt\u002Fstatic\u002F1639777812",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fln\u002F",assetsPath:"\u002Fln\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9295713"}}}}(null));
