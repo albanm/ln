32ae48f194d1ed58f9a33417e3e165048e2ed0c5
Alban Mouton <alban.mouton@gmail.com>
2021-12-19 16:55:00 +0100

    small french errors

---
+++ pages/fiverr-image.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container id="section-head" fluid class="pa-0">
+    <v-card tile color="primary" dark>
+      <v-row class="fill-height" no-gutters>
+        <v-col class="pa-0" cols="6">
+          <v-img :src="basePath + 'Flag_of_France_(1794–1815,_1830–1958).svg'" height="300" />
+        </v-col>
+        <v-col cols="6" class="text-center" align-self="center">
+          <p class="secondary--text text-h3 mt-16">
+            PROOFREADING & EDITING
+          </p>
+        </v-col>
+      </v-row>
+      <v-row class="fill-height" no-gutters>
+        <v-col class="pa-0" cols="6">
+          <v-img :src="basePath + 'student-gacec86f97_1920.jpg'" height="300" />
+        </v-col>
+        <v-col cols="6" class="text-center" align-self="center">
+          <p class="secondary--text text-h3 mb-16">
+            RELECTURE & CORRECTION
+          </p>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    basePath () {
+      return process.env.basePath
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
