0ec6e421ba1bb32063479551e100aa4cf9872f10
Alban Mouton <alban.mouton@gmail.com>
2022-01-29 23:32:00 +0100

    travail sur les images pour fiverr et 5euros

--- pages/5euros-image.vue
+++ pages/5euros-image.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-container id="section-head" fluid class="pa-0">
-    <v-card tile color="primary" dark>
-      <v-img :src="basePath + 'student-gacec86f97_1920.jpg'" height="708">
-        <v-row class="banner">
-          <v-col cols="8" class="text-center" align-self="center">
-            <p class="secondary--text text-h1 mb-0" style="line-height: 120px;font-weight:500;">
-              RELECTURE CORRECTION
-            </p>
-          </v-col>
-          <v-col cols="4" class="text-center" align-self="center">
-            <v-avatar width="200" height="200">
-              <v-img :src="basePath + 'profile.png'" />
-            </v-avatar>
-          </v-col>
-        </v-row>
-      </v-img>
+  <v-container fluid class="euros-image pa-0">
+    <v-card tile color="primary" dark flat>
+      <v-img :src="basePath + 'student-gacec86f97_1920.jpg'" height="708" />
+      <div class="banner-background" />
+      <v-row class="banner">
+        <v-col cols="8" class="text-center" align-self="center">
+          <p class="secondary--text text-h1 mb-0" style="line-height: 120px;font-weight:500;">
+            RELECTURE CORRECTION
+          </p>
+        </v-col>
+        <v-col cols="4" class="text-center" align-self="center">
+          <v-avatar width="200" height="200">
+            <v-img :src="basePath + 'profile.png'" />
+          </v-avatar>
+        </v-col>
+      </v-row>
     </v-card>
   </v-container>
 </template>
@@ -30,14 +30,27 @@ export default {
 </script>
 
 <style>
-.banner {
-  margin-top: 150px;
+.euros-image {
+  width: 1260px;
+  height: 708px;
+}
+
+.euros-image .banner-background {
+  position: absolute;
+  top: 150px;
   height: 408px;
+  width: 100%;
   background: linear-gradient(90deg,
     var(--v-primary-darken4) 0%,
     var(--v-primary-darken2) 60%,
     var(--v-primary-lighten4) 100%
   );
   opacity: 0.9;
+}
+
+.euros-image .banner {
+  position: absolute;
+  top: 150px;
+  height: 408px;
 }
 </style>
--- pages/fiverr-image.vue
+++ pages/fiverr-image.vue
@@ -1,24 +1,32 @@
 <template>
-  <v-container id="section-head" fluid class="pa-0">
-    <v-card tile color="primary" dark>
-      <v-row class="fill-height" no-gutters>
-        <v-col class="pa-0" cols="6">
-          <v-img :src="basePath + 'Flag_of_France_(1794–1815,_1830–1958).svg'" height="300" />
-        </v-col>
-        <v-col cols="6" class="text-center" align-self="center">
-          <p class="secondary--text text-h3 mt-16">
-            PROOFREADING & EDITING
-          </p>
-        </v-col>
-      </v-row>
-      <v-row class="fill-height" no-gutters>
-        <v-col class="pa-0" cols="6">
-          <v-img :src="basePath + 'student-gacec86f97_1920.jpg'" height="300" />
-        </v-col>
-        <v-col cols="6" class="text-center" align-self="center">
-          <p class="secondary--text text-h3 mb-16">
-            RELECTURE & CORRECTION
-          </p>
+  <v-container class="fiverr-image pa-0" fluid>
+    <v-card tile flat dark>
+      <v-img :src="basePath + 'student-gacec86f97_1920.jpg'" height="1076" />
+      <div class="banner-background" />
+      <v-row class="banner">
+        <v-col>
+          <v-row style="height:338px;">
+            <v-col class="text-center message" align-self="center">
+              <p class="secondary--text text-h1 mt-16" style="line-height: 120px;font-weight:500;">
+                PROOFREADING EDITING
+              </p>
+            </v-col>
+            <v-col cols="4" class="text-center" align-self="center">
+              <v-avatar width="200" height="200">
+                <v-img :src="basePath + 'profile.png'" />
+              </v-avatar>
+            </v-col>
+          </v-row>
+          <v-row style="height:338px;">
+            <v-col class="text-center message" align-self="center">
+              <p class="secondary--text text-h1 mb-8" style="line-height: 120px;font-weight:500;">
+                RELECTURE CORRECTION
+              </p>
+            </v-col>
+            <v-col cols="4" class="text-center" align-self="center">
+              <v-img :src="basePath + 'Flag_of_France_(1794–1815,_1830–1958).svg'" height="338px" />
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-card>
@@ -36,5 +44,27 @@ export default {
 </script>
 
 <style>
+.fiverr-image {
+  width: 1600px;
+  height: 1076px;
+}
 
+.fiverr-image .banner-background {
+  position: absolute;
+  top: 200px;
+  height: 676px;
+  width: 100%;
+  background: linear-gradient(90deg,
+    var(--v-primary-darken4) 0%,
+    var(--v-primary-darken2) 60%,
+    var(--v-primary-lighten4) 100%
+  );
+  opacity: 0.9;
+}
+
+.fiverr-image .banner {
+  position: absolute;
+  top: 200px;
+  height: 676px;
+}
 </style>
