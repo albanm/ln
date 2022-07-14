78a1aee51bac5fd94dbff6a4f3b2887274366d5e
albanm <albanm@users.noreply.github.com>
2022-07-14 15:37:52 +0000

    deploy: 97eb9f80b119c2a259910743b1331dc5c2d6f95c

--- _nuxt/static/1657812753/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcgv","\u002Fen","\u002Fen\u002F5euros-image","\u002F5euros-image","\u002Ffiverr-image","\u002Fmentions-legales","\u002Fen\u002Ffiverr-image","\u002Fen\u002Fmentions-legales","\u002Fen\u002Fcgv"]})
